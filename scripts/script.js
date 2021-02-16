013a7dfe0d960eec624e8ac417ab12e792e9a4a2
ayooB-17 <71071391+ayooB-17@users.noreply.github.com>
2021-02-16 13:57:13 +0200

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+//js function to calculate total cost//
+function add_to_total(el) {
+  var parent = $(el).closest("tr");
+  var price =
+    parent.find(".price").val() == "" ? 1 : parent.find(".price").val();
+  var qty = parent.find(".qty").val() == "" ? 1 : parent.find(".qty").val();
+  var total = price * qty;
+  parent.find(".total_price").val(total.toFixed(2));
+}
+
+function add(a, b) {
+  var sum = parseFloat(a) + parseFloat(b);
+  alert("Your Order Cost's: R" + sum.toFixed(2));
+}
